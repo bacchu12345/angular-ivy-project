3ec9d1188327e388f28ce6bdb13ceb6df5f16ce6
bacchu12345 <ninawe.prachi@gmail.com>
2021-12-21 12:06:09 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,23 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInint {
+  loadedPosts = [];
   name = 'Angular ' + VERSION.major;
-  
+  constructor(private http: HttpClient) {}
+  ngOnInint() {
+    this.onFetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {}
+  onFetchPosts() {
+    this.http
+      .get('http://34.135.235.174/user/retreivedata')
+      .subscribe((posts) => {
+        console.log(posts);
+      });
+  }
+  onClearPosts() {}
 }
